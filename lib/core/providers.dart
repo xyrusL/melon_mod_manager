9ffb9062f98944c0ee2cd497f9d82d05be0156b8
xyrusL <91442589+xyrusL@users.noreply.github.com>
2026-02-15 20:49:16 +0800

    chore: update lib/core/providers.dart

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -1,18 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../data/services/github_api_client.dart';
 import '../data/repositories/modrinth_mapping_repository_impl.dart';
 import '../data/repositories/modrinth_repository_impl.dart';
 import '../data/repositories/settings_repository_impl.dart';
 import '../data/services/file_install_service.dart';
 import '../data/services/minecraft_path_service.dart';
+import '../data/services/minecraft_version_service.dart';
 import '../data/services/mod_scanner_service.dart';
 import '../data/services/modrinth_api_client.dart';
 import '../domain/repositories/modrinth_mapping_repository.dart';
 import '../domain/repositories/modrinth_repository.dart';
 import '../domain/repositories/settings_repository.dart';
+import '../domain/entities/developer_snapshot.dart';
 import '../domain/services/dependency_resolver_service.dart';
 import '../domain/usecases/install_mod_usecase.dart';
 import '../domain/usecases/install_queue_usecase.dart';
@@ -44,12 +48,45 @@ final modrinthApiClientProvider = Provider<ModrinthApiClient>((ref) {
   return ModrinthApiClient(client: ref.watch(httpClientProvider));
 });
 
+final gitHubApiClientProvider = Provider<GitHubApiClient>((ref) {
+  return GitHubApiClient(client: ref.watch(httpClientProvider));
+});
+
+const _developerGitHubUsername = 'xyrusL';
+const _projectRepositoryName = 'melon_mod_manager';
+const projectRepositoryUrl =
+    'https://github.com/$_developerGitHubUsername/$_projectRepositoryName';
+
+final developerSnapshotProvider =
+    FutureProvider.autoDispose<DeveloperSnapshot>((ref) async {
+  final api = ref.watch(gitHubApiClientProvider);
+  final profile = await api.getUserProfile(_developerGitHubUsername);
+  final repo = await api.getRepository(
+    _developerGitHubUsername,
+    _projectRepositoryName,
+  );
+  return DeveloperSnapshot(
+    profile: profile.toEntity(),
+    repository: repo.toEntity(),
+  );
+});
+
+final appVersionLabelProvider = FutureProvider.autoDispose<String>((ref) async {
+  final info = await PackageInfo.fromPlatform();
+  return 'v${info.version} (Beta)';
+});
+
 final modrinthRepositoryProvider = Provider<ModrinthRepository>((ref) {
   return ModrinthRepositoryImpl(ref.watch(modrinthApiClientProvider));
 });
 
 final minecraftPathServiceProvider = Provider<MinecraftPathService>((ref) {
   return MinecraftPathService();
+});
+
+final minecraftVersionServiceProvider =
+    Provider<MinecraftVersionService>((ref) {
+  return MinecraftVersionService();
 });
 
 final modScannerServiceProvider = Provider<ModScannerService>((ref) {
